f2bf29328ec03451bee977b780c721cb76a4e49b
= <=>
2024-09-27 15:43:05 +0200

    fixing graphic launcher

--- version_graphique/lanceur_graphique.sh
+++ version_graphique/lanceur_graphique.sh
@@ -7,5 +7,14 @@ else
   ARG=""
 fi
 
+if [ -d "lib" ] && [ "$(ls -A lib)" ]; then
+  echo "lancement"
+else
+  wget https://download2.gluonhq.com/openjfx/17.0.12/openjfx-17.0.12_linux-x64_bin-sdk.zip
+  unzip openjfx-17.0.12_linux-x64_bin-sdk.zip
+  mv javafx-sdk-17.0.12/lib lib
+  rm -rf javafx-sdk-17.0.12
+  rm -rf openjfx-17.0.12_linux-x64_bin-sdk.zip
+fi
 # Exécute la commande java avec l'argument
 java --module-path lib --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing -cp out Main $ARG
